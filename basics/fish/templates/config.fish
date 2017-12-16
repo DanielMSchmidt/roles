f36033fb08b359d3125bdc0e93e08b412aa9f072
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-12-16 11:43:35 +0100

    add configs for shells

--- basics/fish/templates/config.fish
+++ basics/fish/templates/config.fish
@@ -8,8 +8,7 @@ set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
 set -x PATH /usr/local/sbin $PATH
-set -x EDITOR /usr/local/bin/atom
-
+set -x EDITOR {{ theme }}
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
